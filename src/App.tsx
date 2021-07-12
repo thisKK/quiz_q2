4b9e9a24f5f2d9e229038a2c9f2d687cd9c81245
thisKK <sonicrat12@gmail.com>
2021-07-12 22:41:43 +0700

    call api and showing data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+interface data {
+  includes(searchInput: string): string;
+  index: number;
+  name: number;
+}
 
 function App() {
+  const [data, setData] = useState<data[]>([]);
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const getdata = () => {
+    axios.get(" https://api.publicapis.org/categories").then((response) => {
+      setData(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input
+          style={{ width: "500px" }}
+          placeholder="Enter name for fillter"
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        ></input>
+        <table
+          style={{
+            textAlign: "left",
+            width: "30%",
+          }}
         >
-          Learn React
-        </a>
+          <tbody>
+            {data!
+              .filter((name) => name.includes(searchInput))
+              .map((name, index) => (
+                <tr key={index}>
+                  <th>
+                    <li>{name}</li>
+                  </th>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
